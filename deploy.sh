72e0abc95576cff716381d43998f58b00fbfe61a
WhyJaz <1522213037@qq.com>
2021-08-15 22:47:45 +0800

    add bash

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:WhyJaz/vuepress-doc.git master:gh-pages
+git push -f git@github.com:WhyJaz/vuepress-doc.git blogs:gh-pages
 
 cd -
