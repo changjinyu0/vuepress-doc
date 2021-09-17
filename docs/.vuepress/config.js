c913538e8975d5790c055427c21789759431fa96
wanghuaiyong <wanghuaiyong@croot.com>
2021-09-17 21:02:30 +0800

    add readme.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   base: '/vuepress-doc/', // 部署站点的基础路径
-  title: '前端面试必备',
+  title: '前端装逼大全',
   description: 'Just playing around',
   displayAllHeaders: true,
   markdown: {
@@ -8,23 +8,23 @@ module.exports = {
   },
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/contents/JS/基础类型.md' },
-      { text: 'Guide', link: '/guide/' },
-      {
-        text: 'Languages',
-        items: [
-          { text: 'Chinese', link: '/language/chinese' },
-          { text: 'English', link: '/language/english' }
-        ]
-      },
-      { text: 'External', link: 'https://www.baidu.com' },
+      { text: 'Home', link: '/contents/js/基础类型.md' },
+      { text: 'Guide', link: '/contents/js/基础类型.md' },
+      // {
+      //   text: 'Languages',
+      //   items: [
+      //     { text: 'Chinese', link: '/language/chinese' },
+      //     { text: 'English', link: '/language/english' }
+      //   ]
+      // },
+      { text: 'External', link: 'https://github.com/WhyJaz/vuepress-doc' },
     ],
     sidebar: [
       {
         title: 'JS',
         children: [
-          ['/contents/JS/基础类型.md', '基础类型'],
-          ['/contents/JS/闭包.md', '闭包'],
+          ['/contents/js/基础类型.md', '基础类型'],
+          ['/contents/js/闭包.md', '闭包'],
         ]
       },
     ]
