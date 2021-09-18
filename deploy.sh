26610c7f84f0038d7908574f49737b415f674300
changjinyu <changjinyu@croot.com>
2021-09-18 12:33:21 +0800

    测试

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:WhyJaz/vuepress-doc.git master:gh-pages
+git push -f git@github.com:changjinyu0/vuepress-doc.git master:gh-pages
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 
 cd -
